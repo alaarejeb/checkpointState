7c084d54509c142ba34a87203bdfe4ef28f8211d
Alla Rejeb <alaa-rejeb@hotmail.fr>
2023-11-30 13:54:43 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      person: {
+        fullName: 'Alaa Rejeb',
+        bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        imgSrc: 'https://t3.ftcdn.net/jpg/02/43/12/34/360_F_243123463_zTooub557xEWABDLk0jJklDyLSGl2jrr.jpg',
+        profession: 'webdevelloper',
+      },
+      show: false,
+      mountTime: null,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ mountTime: new Date() });
+
+    this.interval = setInterval(() => {
+      this.forceUpdate(); 
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  toggleShow = () => {
+    this.setState((prevState) => ({ show: !prevState.show }));
+  };
+
+  render() {
+    const { person, show, mountTime } = this.state;
+
+    return (
+      <div>
+        <h1>Click on the button</h1>
+        <button onClick={this.toggleShow}>toggle</button>
+
+        {show && (
+          <div>
+            <h2>{person.fullName}</h2>
+            <p>{person.bio}</p>
+            <p>Profession: {person.profession}</p>
+            <img src={person.imgSrc} alt="Person" />
+
+            <p>Time: {Math.floor((new Date() - mountTime) / 1000)} seconds</p>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
